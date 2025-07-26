d9d3ee1ed67de494f6bf4e1b59bd93c5a4c1ca59
Iloveher3000 <hetumedhu@gmail.com>
2025-07-26 23:36:48 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,4 @@
-const isProd = process.env.NODE_ENV === 'production';
-
-const nextConfig = {
-  // Export the site as static HTML for GitHub Pages
-  output: 'export',
-  
-  // Disable Next.js image optimization for static exports
-  images: {
-    unoptimized: true,
-  },
-
-  // Tell Next.js to prefix all asset and route URLs with your repo name
-  basePath: isProd ? '/HappyBirthday' : '',
-  assetPrefix: isProd ? '/HappyBirthday/' : '',
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
 export default nextConfig;
